9c3d53cb0bb49f9cc01743e3d63ae37e23e459a6
black <cool1726@naver.com>
2020-09-01 02:02:27 +0900

    navbar in home.html, css 파일 추가

--- ggjProject/ggjProject/settings.py
+++ ggjProject/ggjProject/settings.py
@@ -120,7 +120,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'home', 'static')
+    os.path.join(BASE_DIR, 'home', 'static'),
+    os.path.join(BASE_DIR, 'account', 'static')
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
