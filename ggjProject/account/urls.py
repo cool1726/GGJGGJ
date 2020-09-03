e0b6f7ae5e59ea8f51a679ae4b17402ffb3b39fd
huhsaeng <huh5477@naver.com>
2020-09-03 22:21:52 +0900

    huhsaeng_edit

--- ggjProject/account/urls.py
+++ ggjProject/account/urls.py
@@ -3,4 +3,5 @@ from .views import signup, login
 urlpatterns = [
     path('back_signup/', signup, name='back_signup'),
     path('back_login/', login, name='back_login'),
+    path('signup/', signup, name='signup'),
 ]
