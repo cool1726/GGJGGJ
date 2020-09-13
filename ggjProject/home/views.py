ad347371a02e535b674ecc334d3ed83ce6879361
black <cool1726@naver.com>
2020-09-14 00:05:58 +0900

    new.html img upload editted

--- ggjProject/home/views.py
+++ ggjProject/home/views.py
@@ -19,7 +19,7 @@ def home(request):
 """back_new.html로 이동"""
 def new(request):
     bookShelves = BookShelf.objects.all()
-    return render(request, 'back_new.html', {'bookShelves': bookShelves})
+    return render(request, 'new.html', {'bookShelves': bookShelves})
 
 """
 post create 함수
