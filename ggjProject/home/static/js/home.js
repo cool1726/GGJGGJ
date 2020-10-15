2af95cbde59b10607caa524186d66fa9ca2f2670
black <cool1726@naver.com>
2020-10-16 00:16:22 +0900

    home.js 한번 클릭하면 열렸다 닫히는 레이아웃 만드는 중(70%)

--- ggjProject/home/static/js/home.js
+++ ggjProject/home/static/js/home.js
@@ -9,26 +9,82 @@ const openPost = () => {
     closedPost.classList.add("hidden");
 }
 
+let openPostList = [];
+
 posts.forEach(post => {
     post.addEventListener("click", () => {
         //console.log(post.children);
         const closedPost = post.children[0];
         const openedPost = post.children[1];
-        const location = closedPost.style.transform;
-        openedPost.style.transform = location;
-        console.log()
+
+        //closedPosts[i].style.transform = `translateX(${x}px) translateY(${y}px)`;
 
         closedPost.style.display = "none";
-        closedPost.classList.add("hidden");
-        openedPost.classList.remove("hidden");
+        closedPost.classList.toggle("hidden");
+
+        const location = closedPost.style.transform;
+        openedPost.style.transform = location;
+        openedPost.style.display = 'inline-block';
+        openedPost.classList.toggle("hidden");
+
+        posts.forEach(xypost => {
+            if (xypost != post) {
+                // console.log(getTranslateXValue(xypost.children[0].style.transform));
+                let split = xypost.children[0].style.transform.split(' ');
+                let x = getTranslateXValue(split[0]);
+                let y = getTranslateYValue(split[1]);
+
+                // console.log(xypost.children[0].style.transform);
+                console.log("x, y : ", x, y);
+
+                const h = parseInt(openedPost.style.height);
+
+                console.log(h);
+
+                const clickX = getTranslateXValue(openedPost.style.transform);
+                const clickY = getTranslateYValue(openedPost.style.transform);
+
+                console.log(clickY);
+
+                if (x >= clickX && x <= clickX + 573 && y >= clickY) { // 클릭한 카드가 펼쳐질 때 그 아래에 쌓이는 카드들
+                    y = y + h;
+                    xypost.children[0].style.transform = `translateX(${x}px) translateY(${y}px)`;
+                    //console.log(xypost.children[0].children[2], xypost.children[0].style.transform);
+
+                    // console.log("wow");
+                    // if (y >= clickY && y <= clickY + 456) {
+                    //console.log("wow! " + (y + h));
+                    // }
+                }
+                // console.log(getTranslateYValue(openedPost.style.transform));
+                // if (getTranslateYValue(xypost.style.transform) > getTranslateYValue(openedPost.style.transform)) {
+                //     console.log(getTranslateYValue(xypost.style.transform));
+                // }
+            }
+
+        })
+
     });
 });
 
-const strToDate = (obj) => {
-    let year = obj.substring(0, 4);
-    let month = obj.substring(5, 7);
-    let day = obj.substring(8, 10);
-    return `${year}.${month}.${day}`;
+// 해당 개체의 X 좌표값을 구하는 함수
+function getTranslateXValue(translateString) {
+    let n = translateString.indexOf("(");
+    let n1 = translateString.indexOf(",");
+
+    let res = parseInt(translateString.slice(n + 1, n1 - 2));
+
+    return res;
+}
+
+// 해당 개체의 Y 좌표값을 구하는 함수
+function getTranslateYValue(translateString) {
+    let n = translateString.indexOf("(");
+    let n1 = translateString.indexOf(",");
+
+    let res = parseInt(translateString.slice(n + 1, n1));
+
+    return res;
 }
 
 // 각 글 위치는 imgStack이라는 배열을 만들어 계산함 (Column의 개수에 해당하는 길이를 가짐)
@@ -51,7 +107,7 @@ const resizeApply = () => {
     const bottomMargin = 15;
     const minHeight = 160;
 
-    console.log(imgStack);
+    // console.log(imgStack);
 
     for (let i = 0; i < closedPosts.length; i++) {
         let minIndex = imgStack.indexOf(Math.min.apply(0, imgStack));
@@ -75,13 +131,9 @@ const resizeApply = () => {
         // console.log(opacity);
         //console.log(imgStack);
         if (i == closedPosts.length - 1) {
-            document.querySelector(".masonry-container").style.height = `${Math.max.apply(0, imgStack)}px`;
+            document.querySelector("#masonry-container").style.height = `${Math.max.apply(0, imgStack)}px`;
         }
     }
-}
-
-const firstClick = () => {
-
 }
 
 window.onload = function () {
@@ -91,3 +143,11 @@ window.onload = function () {
 }
 
 resizeApply();
+
+
+const strToDate = (obj) => {
+    let year = obj.substring(0, 4);
+    let month = obj.substring(5, 7);
+    let day = obj.substring(8, 10);
+    return `${year}.${month}.${day}`;
+}
