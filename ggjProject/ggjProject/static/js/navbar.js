0594240b37cb6468edd1fad79673c02bc97443e6
huhsaeng <huh5477@naver.com>
2020-10-23 02:00:25 +0900

    huh

--- ggjProject/ggjProject/static/js/navbar.js
+++ ggjProject/ggjProject/static/js/navbar.js
@@ -1,8 +1,21 @@
-const navbarWelcome = document.querySelector(".navbar-welcome");
+// const navbarWelcome = document.querySelector(".navbar-welcome");
 const navbarMyMenu = document.querySelector(".navbar-mymenu");
+const profileBtn = document.getElementById("profile");
 
 const toggleMyMenu = () => {
     console.log("profile clicked");
     navbarMyMenu.classList.toggle("hidden");
 };
-navbarWelcome.addEventListener("click", toggleMyMenu);
+
+const openCloseModal = () => {
+    console.log("check");
+    navbarMyMenu.classList.toggle("hidden");
+
+  };
+
+const openModal = () => {
+    modal.classList.remove("hidden");
+    body.classList.add("not-scroll");
+  };
+// navbarWelcome.addEventListener("click", toggleMyMenu);
+profileBtn.addEventListener("click", openCloseModal);
